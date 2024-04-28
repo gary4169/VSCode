f567a0b5a23467482aa0571606d27b99c9aeb1b7
gary1012020114 <gary1012020114@gmail.com>
2024-04-28 16:51:46 +0800

    Initial commit

---
+++ Program2.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program2
+{
+    static void Main()
+    {
+        bool flag = true;
+        if (flag)
+        {
+            int value = 10;
+            Console.WriteLine($"Inside the code block: {value}");
+        }
+    }
+}
